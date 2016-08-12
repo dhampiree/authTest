ef940016a5fafd7353a33fbece5b42b0dbd5596c
Anton <anton@iMac-Anton.local>
2016-08-12 18:36:44 +0300

    First commit

---
+++ DSAuthTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DSAuthTest
+//
+//  Created by Anton on 12.08.16.
+//  Copyright © 2016 DhampireeSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

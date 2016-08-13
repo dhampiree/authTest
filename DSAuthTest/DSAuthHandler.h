a9bef5a9995cc390af0b8d12fbf9e3ecacaf1e85
Anton <anton@iMac-Anton.local>
2016-08-13 15:10:38 +0300

    Auth class added

---
+++ DSAuthTest/DSAuthHandler.h
@@ -0,0 +1,13 @@
+//
+//  DSAuthHandler.h
+//  DSAuthTest
+//
+//  Created by Anton on 12.08.16.
+//  Copyright © 2016 DhampireeSoftware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSAuthHandler : NSObject
+
+@end
---
+++ DSAuthTest/DSAuthHandler.m
@@ -0,0 +1,13 @@
+//
+//  DSAuthHandler.m
+//  DSAuthTest
+//
+//  Created by Anton on 12.08.16.
+//  Copyright © 2016 DhampireeSoftware. All rights reserved.
+//
+
+#import "DSAuthHandler.h"
+
+@implementation DSAuthHandler
+
+@end
